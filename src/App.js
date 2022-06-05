2f67310517674a882276606266efc6231db4c2e5
Masum-Arifin <masumarifinbd@gmail.com>
2022-06-06 05:34:52 +0600

    new code

--- src/App.js
+++ src/App.js
@@ -1,41 +1,41 @@
-import {
-    useEffect
-} from "react";
-import Home from "./components/Home";
-import "./components/styles/main.scss";
-import {
-    Toaster
-} from "react-hot-toast";
-import {
-    gsap
-} from "gsap";
+// import {
+//     useEffect
+// } from "react";
+// import Home from "./components/Home";
+// import "./components/styles/main.scss";
+// import {
+//     Toaster
+// } from "react-hot-toast";
+// import {
+//     gsap
+// } from "gsap";
 
-const App = () => {
-    useEffect(() => {
-        gsap.to(".wrapper", {
-            visibility: "visible",
-            autoAlpha: 1
-        });
-    }, []);
+// const App = () => {
+//     useEffect(() => {
+//         gsap.to(".wrapper", {
+//             visibility: "visible",
+//             autoAlpha: 1
+//         });
+//     }, []);
 
-    return ( < div style = {{visibility: "hidden"}}className = 'wrapper' >
-        <
-        Toaster position = 'bottom-right'
-        toastOptions = {
-            {
-                // className: "",
-                duration: 2000,
-                style: {
-                    background: "#2d2d2d",
-                    color: "#f0f0f0",
-                    borderRadius: 0,
-                },
-            }
-        }
-        /> <
-        Home / >
-        </div>
-    );
-};
+//     return ( < div style = {{visibility: "hidden"}}className = 'wrapper' >
+//         <
+//         Toaster position = 'bottom-right'
+//         toastOptions = {
+//             {
+//                 // className: "",
+//                 duration: 2000,
+//                 style: {
+//                     background: "#2d2d2d",
+//                     color: "#f0f0f0",
+//                     borderRadius: 0,
+//                 },
+//             }
+//         }
+//         /> <
+//         Home / >
+//         </div>
+//     );
+// };
 
-export default App;
+// export default App;
